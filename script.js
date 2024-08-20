1ff4e39bd3d7f36b5cb2a24ec142d974d4833744
Jmara <jmara.0923@gmail.com>
2024-08-20 17:52:01 +0000

    feat: add function for computer and human choice

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello, world");
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3) + 1;
+
+  if (random === 1) {
+    return "rock";
+  } else if (random === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let choice = prompt("Rock, Paper, or Scissors?");
+  choice = choice.toLowerCase();
+
+  if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+    alert(
+      "Invalid input. Please choose one of the following: rock, paper, scissors."
+    );
+    getHumanChoice();
+  } else {
+    return choice;
+  }
+  console.log(choice);
+}
+
+getComputerChoice();
+getHumanChoice();
+console.log(getComputerChoice());
